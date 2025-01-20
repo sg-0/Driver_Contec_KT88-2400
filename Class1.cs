239604a9c24d9068de91403c3e3efdfa452ce164
gsandro <sandro.gantenbein@pm.me>
2025-01-20 12:14:11 +0100

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contec_KT88_2400
+{
+    public class Class1
+    {
+    }
+}
